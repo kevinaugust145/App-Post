fb4eb1c74ada6f0018d210f86958a78228fe1815
KevinHung <kevinaugust145@gmail.com>
2024-07-15 08:30:44 +0800

    Initial Commit

---
+++ App PostTests/App_PostTests.swift
@@ -0,0 +1,17 @@
+//
+//  App_PostTests.swift
+//  App PostTests
+//
+//  Created by 洪宗燦 on 2024/7/15.
+//
+
+import Testing
+@testable import App_Post
+
+struct App_PostTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
